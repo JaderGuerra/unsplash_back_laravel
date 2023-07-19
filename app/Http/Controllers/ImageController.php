b675991ef75914f7fb4fcb1b1b5bdde758f0149a
Jader <swilor29@gmail.com>
2023-07-18 19:33:08 -0500

    feat: :sparkles: send file

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -2,9 +2,29 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\PostCreateRequest;
+use App\Models\File;
 
 class ImageController extends Controller
 {
-    //
+    public function uploadImage(PostCreateRequest $request)
+    {
+
+        $label = $request->input('label');
+        $image = $request->file('image_path');
+
+        $imagePath = $image->store('image_path', 'public');
+
+        $file = new File();
+        $file->label = $label;
+        $file->image_path = $imagePath;
+        $file->save();
+
+        $imageName = basename($imagePath);
+
+        return response()->json([
+            'message'       => 'Imagen cargada exitosamente',
+            'image_name'    =>  $imageName,
+        ], 200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ImageController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,4 +19,4 @@ use Illuminate\Support\Facades\Route;
     return $request->user();
 }); */
 
-Route::post('/upload-image', 'ImageController@uploadImage');
+Route::post('uploadfile', [ImageController::class, 'uploadImage']);
